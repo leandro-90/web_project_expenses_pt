337035cd1cffe2733be5341fc923db3dd00bad40
Leandro <ferreir720@gmail.com>
2026-08-21 23:40:07 -0300

    Nova atualização: Novas variáveis adicionadas

---
+++ scripts/calculations.js
@@ -0,0 +1,12 @@
+let budgetValue = 0; // Armazena o orçamento.
+let totalExpensesValue = 0; // Armazena as despesas totais.
+
+let expenseEntries = [
+  [groceries, 33],
+  [restaurants, 50],
+  [transport, 12],
+  [home, 70],
+  [subscriptions, 14],
+  [groceries, 28],
+  [subscriptions, 12],
+]; // Categoria das despesas.
